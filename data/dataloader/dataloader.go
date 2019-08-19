5819d473d4bd8657d4cbf0db5ab5df13bae2e9c8
chengqinglian <chengqinglian@qq.com>
2019-08-19 14:57:38 +0800

    nothing

--- data/dataloader/dataloader.go
+++ data/dataloader/dataloader.go
@@ -7,9 +7,16 @@ import (
 	streamer2 "mtggokits/data/dataloader/streamer"
 )
 
-var DataStreamers = make(map[string]streamer2.DataStreamer)
 
-func Get(name string, key container.MapKey) (interface{}, error) {
+type Loader struct {
+	DataStreamers map[string]streamer2.DataStreamer
+}
+
+func NewLoader() *Loader {
+	return Loader{DataStreamers:new(map[string]streamer2.DataStreamer))}
+}
+
+func(* loader) Get(name string, key container.MapKey) (interface{}, error) {
 	s, ok := DataStreamers[name]
 	if !ok {
 		return nil, errors.New("not found streamer[" + name + "]")
@@ -21,12 +28,10 @@ func Get(name string, key container.MapKey) (interface{}, error) {
 	return c.Get(key)
 }
 
-func Register(name string, streamer streamer2.DataStreamer) error {
+func(* loader) Register(name string, streamer streamer2.DataStreamer) error {
 	if _, ok := DataStreamers[name]; ok {
-		fmt.Println("abcdefg")
 		return errors.New("streamer[" + name + "] has already exist")
 	}
-	fmt.Println("Register: ", name)
 	DataStreamers[name] = streamer
 	return nil
 }
