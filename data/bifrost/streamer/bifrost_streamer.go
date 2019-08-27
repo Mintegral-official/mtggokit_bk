636fe4d7f9e4103dfdba0dfd88055fc3641c1d33
wangjia <jia.wang@mintegral.com>
2019-08-27 16:05:49 +0800

    add email into travis

--- data/bifrost/streamer/bifrost_streamer.go
+++ data/bifrost/streamer/bifrost_streamer.go
@@ -1,1 +1,22 @@
 package streamer
+
+import "github.com/Mintegral-official/mtggokit/data/container"
+
+type BiFrostStreamerCfg struct {
+	Name         string
+	IpPort       string
+	BaseFilePath string
+}
+
+type BiFrostStreamer struct {
+	Cfg       *BiFrostStreamerCfg
+	container container.Container
+}
+
+func NewBiFrostStreamer(cfg *BiFrostStreamerCfg) *BiFrostStreamer {
+	return &BiFrostStreamer{Cfg: cfg}
+}
+
+func UpdateData() {
+	//先加载基准，然后后台增量
+}
--- data/container/concurrent_map.go
+++ data/container/concurrent_map.go
@@ -1,1 +1,5 @@
 package container
+
+import "github.com/easierway/concurrent_map"
+
+var m concurrent_map.ConcurrentMap
