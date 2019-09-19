c38bd63c1be6bfbeb71c4ec4b7101bf373f5a66e
wangjia <jia.wang@mintegral.com>
2019-09-19 13:41:37 +0800

    delete msgpack codec test

--- bifrost/streamer/streamer_provider_test.go
+++ bifrost/streamer/streamer_provider_test.go
@@ -28,33 +28,33 @@ func (c MsgpackCodec) Decode(data []byte, i interface{}) error {
 	return err
 }
 
-func TestMsgCodec(t *testing.T) {
-	base := &BaseInfo{
-		Name:     "bifrost_streamer_example",
-		Progress: 5,
-		Data: map[container.MapKey]interface{}{
-			container.StrKey("1"): 1,
-			container.StrKey("2"): 4,
-		},
-	}
-
-	codec := MsgpackCodec{}
-
-	data, err := codec.Encode(base)
-	if err != nil {
-		fmt.Println("Encode error, ", err.Error())
-		return
-	}
-	fmt.Println("Encode succ: ", len(data))
-	n := &BaseInfo{}
-	err = codec.Decode(data, n)
-	if err != nil {
-		fmt.Println("Decode error, ", err.Error())
-		return
-	}
-
-	fmt.Println("Decode succ, ", n)
-}
+//func TestMsgCodec(t *testing.T) {
+//	base := &BaseInfo{
+//		Name:     "bifrost_streamer_example",
+//		Progress: 5,
+//		Data: map[container.MapKey]interface{}{
+//			container.StrKey("1"): 1,
+//			container.StrKey("2"): 4,
+//		},
+//	}
+//
+//	codec := MsgpackCodec{}
+//
+//	data, err := codec.Encode(base)
+//	if err != nil {
+//		fmt.Println("Encode error, ", err.Error())
+//		return
+//	}
+//	fmt.Println("Encode succ: ", len(data))
+//	n := &BaseInfo{}
+//	err = codec.Decode(data, n)
+//	if err != nil {
+//		fmt.Println("Decode error, ", err.Error())
+//		return
+//	}
+//
+//	fmt.Println("Decode succ, ", n)
+//}
 
 func init() {
 	gob.Register(&container.Int64Key{})
