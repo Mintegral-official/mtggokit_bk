4981fc34abfbbe0a9ff8dbb86244022b83ae4651
Schneizelw <744587537@qq.com>
2019-10-17 19:59:04 +0800

    update

--- bifrost/log/logger.go
+++
@@ -1,9 +0,0 @@
-package log
-
-// Logger for log
-type BiLogger interface {
-	Info(args ...interface{})
-	Warn(args ...interface{})
-	Infof(format string, args ...interface{})
-	Warnf(format string, args ...interface{})
-}
--- bifrost/streamer/streamer_provider_config.go
+++
@@ -1,9 +0,0 @@
-package streamer
-
-import "github.com/Mintegral-official/mtggokit/bifrost/log"
-
-type StreamerProviderCfg struct {
-	Name       string
-	ExpireTime int64
-	Logger     log.BiLogger
-}
